765195b5cea0acd7369532b193e9066b8a1fc98e
Nick Makris <makris@me.com>
2024-06-27 14:08:19 -0400

    Changed vite path

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
-import { fileURLToPath, URL } from "node:url";
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
-import svgLoader from "vite-svg-loader";
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/amu-generator/',
+    base: '/',
     plugins: [vue(), vueJsx(), svgLoader()],
     resolve: {
         alias: {
-            "@": fileURLToPath(new URL("./src", import.meta.url)),
-            "@asset": fileURLToPath(new URL("./src/assets", import.meta.url)),
-            "@component": fileURLToPath(new URL("./src/components", import.meta.url)),
-            "@view": fileURLToPath(new URL("./src/views", import.meta.url)),
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            '@asset': fileURLToPath(new URL('./src/assets', import.meta.url)),
+            '@component': fileURLToPath(new URL('./src/components', import.meta.url)),
+            '@view': fileURLToPath(new URL('./src/views', import.meta.url)),
         },
     },
-});
+})
