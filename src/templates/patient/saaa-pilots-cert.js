b872cc48e56e49e8bfa961186b797d879801d4f4
Nick Makris <makris@me.com>
2024-12-28 17:31:25 -0500

    Fix for jumpmaster

--- src/templates/patient/saaa-pilots-cert.js
+++ src/templates/patient/saaa-pilots-cert.js
@@ -1,38 +1,37 @@
 import {dateToUtc} from '@util/mixins'
 
-export const generateSaaaPilotsCert = (
-    {
-        fullName
-    },
-    {
-        internalRank,
-        name,
-        rank,
-        signature
-    },
-    destination,
-    newPage = false
-) => {
+export const generateSaaaPilotsCert = ({fullName}, {internalRank, name, rank, signature}, destination, newPage = false) => {
     if (!internalRank || !name || !rank || !signature) {
         alert('You need to make sure everything in Settings is filled out')
     } else {
-        const template = `[img]https://i.imgur.com/hM4TXWD.png[/img]
-[align=center]
-[img]https://i.imgur.com/HNP4ksW.png[/img]
+        const template = `[img]https://i.imgur.com/M7FTbHm.png[/img]
+
 [divbox=white]
-[fimg=120,120]https://i.imgur.com/QYXPM0p.png[/fimg]
+[color=transparent]spacer[/color]
+[center][fimg=130,130]https://i.imgur.com/QYXPM0p.png[/fimg]
+[color=transparent]spacer[/color]
+[img]https://i.imgur.com/7x0vy1x.png[/img][/center]
+[color=transparent]spacer[/color]
+[center][size=140]${fullName}[/size]
+[color=transparent]spacer[/color]
+[size=120]was examined by AMU staff on [b]${dateToUtc()}[/b]
+and found medically fit to be educated on and practice as:
+[ ] An Aircraft Pilot
+[ ] A Jumpmaster.[/size]
+
+
 [center][img]https://i.imgur.com/7x0vy1x.png[/img][/center]
-[center][size=140]${fullName}
-was examined by AMU staff on ${dateToUtc()} and found medically fit to operate an aircraft.[/size]
-[center][img]https://i.imgur.com/7x0vy1x.png[/img][/center]
-[/align]
-[divbox=white]
+
+[hr][/hr]
+
+[i][b]Examination performed by:[/b][/i]
 
 [img]${signature}[/img]
 [i]${name}[/i]
 [b]${internalRank} / ${rank}[/b]
 [b]Los Santos Emergency Medical Services[/b]
-[/divbox][img]https://i.imgur.com/HNP4ksW.png[/img]`
+
+[/divbox][lsemsfooter]`
 
         navigator.clipboard.writeText(template).then(
             () => {},
